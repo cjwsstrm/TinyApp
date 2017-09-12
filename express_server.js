46660d5bcfcd7ab379a2c675efc7b3e331b29125
cjwsstrm <cjwesstrom@gmail.com>
2017-09-12 22:58:13 +0000

    added url submission form and linted other files

--- express_server.js
+++ express_server.js
@@ -1,23 +1,34 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 8080;
-
-app.set("view engine", "ejs");
-
-var urlDatabase = {
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+const bodyParser = require("body-parser");
+const urlDatabase = {
   "b2xVn2": "http://lighthouselabs.ca",
   "95m5xK": "http://google.com"
 };
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/urls/new", (request, response) => {
+  response.render("urls_new");
+});
+
+app.post("/urls", (request, response) => {
+  console.log(request.body);  // debug statement to see POST parameters
+  response.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls", (request, response) => {
   let templateVars = { urls: urlDatabase };
   response.render("urls_index", templateVars);
 });
 
-app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id,
-                       keyURL: urlDatabase[req.params.id]};
-  res.render("urls_show", templateVars);
+app.get("/urls/:id", (request, response) => {
+  let templateVars = { shortURL: request.params.id,
+                       keyURL: urlDatabase[request.params.id]};
+  response.render("urls_show", templateVars);
 });
 
 app.get("/", (request, response) => {
@@ -31,6 +42,8 @@ app.get("/urls/json", (request, response) => {
 app.get("/hello", (request, response) => {
   response.end("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
